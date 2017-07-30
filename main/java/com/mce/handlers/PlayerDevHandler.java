91ed4f4c0644289158a6efc369b710f28295a491
MCE626 <Contact.MCE626@gmail.com>
2017-07-30 18:04:02 -0500

    Added upgrade textures, contributors are grabbed from a list on a google doc instead of hard coded in.

--- main/java/com/mce/handlers/PlayerDevHandler.java
+++ main/java/com/mce/handlers/PlayerDevHandler.java
@@ -1,12 +1,17 @@
 package com.mce.handlers;
 
+import java.io.InputStream;
+import java.net.URL;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
-public class PlayerDevHandler {	
+public class PlayerDevHandler {
+	public static final String name = "https://docs.google.com/document/d/1I3vsIBKBeYvLp0XKToGBJWDG0DRpiO_c4KuYRkzKqd4/pub";
+
 	@SubscribeEvent
-	public void changeDevName(PlayerEvent.NameFormat e) {
+	public void changeDevName(PlayerEvent.NameFormat e) throws Exception {
 		if (e.username.equals("MCE626")) {
 			e.displayname = EnumChatFormatting.AQUA + "{Dev} MCE626";
 		}
@@ -22,19 +27,33 @@ public class PlayerDevHandler {
 		else if (e.username.equals("_Guybot1001_")) {
 			e.displayname = EnumChatFormatting.BLUE + "(Dev) _Guybot1001_";
 		}
-		
+
 		// Contributors
-		else if(e.username.equals("MrProBro")){
-			e.displayname = EnumChatFormatting.AQUA + "ᶦ " + EnumChatFormatting.RESET + "MrProBro";
+		else if (e.username.equals(getNames())) {
+			try {
+				e.displayname = EnumChatFormatting.AQUA + "ᶦ " + EnumChatFormatting.RESET + getNames();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
 		}
-		
-		else if(e.username.equals("domi1819")){
-			e.displayname = EnumChatFormatting.AQUA + "ᶦ " + EnumChatFormatting.RESET + "domi1819";
+	}
+
+	public static String getNames() throws Exception {
+		String data = getData("https://docs.google.com/document/d/1I3vsIBKBeYvLp0XKToGBJWDG0DRpiO_c4KuYRkzKqd4/pub");
+		return data.substring(data.indexOf("[u]") + 3, data.indexOf("[/u]"));
+	}
+
+	private static String getData(String address) throws Exception {
+		URL url = new URL(address);
+		InputStream html = null;
+		html = url.openStream();
+
+		int c = 0;
+		StringBuffer buff = new StringBuffer("");
+		while (c != -1) {
+			c = html.read();
+			buff.append((char) c);
 		}
-		
-		// Patreons
-		else if(e.username.equals("ergwghetrgergerg")){ // Test name so I can just copy that generic currency symbol
-			e.displayname = EnumChatFormatting.AQUA + "¤ " + EnumChatFormatting.RESET + "name";
-		}
+		return buff.toString();
 	}
 }
